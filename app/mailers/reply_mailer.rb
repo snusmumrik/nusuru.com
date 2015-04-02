33e12d2f6b73982a6bdcc028ca08609b03ced414
Hiroyuki Kondo <hiroyuki@chishaku.com>
2015-04-02 13:14:37 +0900

    change logo

--- app/mailers/reply_mailer.rb
+++ app/mailers/reply_mailer.rb
@@ -2,6 +2,6 @@
 class ReplyMailer < ApplicationMailer
   def new_reply_email(reply)
     @reply = reply
-    mail(to: @reply.post.user.email, subject: "【ぬするー情報局】新しい情報提供がありました")
+    mail(to: @reply.post.user.email, subject: "【ヌスルー情報局】新しい情報提供がありました")
   end
 end
